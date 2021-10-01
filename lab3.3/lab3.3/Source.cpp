cbb8fdabad8bdc4a285bec4a18ab5c43cd047d50
Yuliia <ylua.vasylyk.04@gmail.com>
2021-10-01 11:28:13 +0300

    hello

--- lab3.3/lab3.3/Source.cpp
+++ lab3.3/lab3.3/Source.cpp
@@ -11,30 +11,32 @@ using namespace std;
 
 int main()
 {
-    double x; // вхідний аргумент
-    double R; // вхідний параметр
-    double y; // результат обчислення виразу
-    cout << "R = "; cin >> R;
-    cout << "x = "; cin >> x;
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
 
-    // розгалуження в повній формі
-    if (x <= -8)
-        y = -R;
-    else
-        if (-8 > x && x <= -R)
-            y = -R + (R / (-R + 8)) * (x + 8);
-        else
-            if (x >= -R && x <= R)
-                y = sqrt(pow(R, 2) - pow(x, 2));
-            else
-                if (x >= R && x <= 5)
-                    y = (2 / (5 - R) * (x - R));
-                else
-                    y = 3;
+	// розгалуження в повній формі
+	if (x <= -8)
+		y = -R;
+	else
+		if (-8 < x && x <= -R)
+			y = -R + ((R / (-R + 8)) * (x + 8));
+		else
+			if (-R < x && x <= R)
+				y = -sqrt((R * R) - (x * x));
+			else
+				if (R < x && x <= 5)
+					y = (2. / (5 - R)) * (x - R);
+				else
+					y = 3;
 
-    cout << endl;
-    cout << "y = " << y << endl;
+	cout << endl;
+	cout << "y = " << y << endl;
 
-    cin.get();
-    return 0;
+	cin.get();
+	return 0;
 }
+
+
